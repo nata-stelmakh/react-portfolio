e3d1131490c863452dd4cabbd2d4dfe2622adcc2
Author Name <stelmakh.natallia@gmail.com>
2020-11-04 20:16:35 -0500

    Changed Resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,19 +5,29 @@ class Resume extends Component {
     return (
       <div>
         <div>
-          <figure className="figure">
+          <div class="demo-card-image mdl-card mdl-shadow--2dp">
+            <div class="mdl-card__title mdl-card--expand"></div>
+            <div class="mdl-card__actions">
+              <span class="demo-card-image__filename">
+                <a href="https://docs.google.com/document/d/1DzwmrQ0G1EpsKRMEtz3tveR-pwi1IDykfRiGPPRrMiw/edit?usp=sharing">
+                  Download
+                </a>
+              </span>
+            </div>
+          </div>
+          {/* <figure className="figure">
             <img
               src="public\assets\Resume.png"
               className="card-img-top figure-img img-fluid rounded"
               alt="organizer"
               width="503"
               height="290"
-            ></img>
+            ></img> */}
 
-            <a href="https://drive.google.com/file/d/1Uvkst4PEBES9FFv8gWFDZBLzElUhI_Nh/view?usp=sharing">
+          {/* <a href="https://drive.google.com/file/d/1Uvkst4PEBES9FFv8gWFDZBLzElUhI_Nh/view?usp=sharing">
               <p className="figcaption">Download</p>
             </a>
-          </figure>
+          </figure> */}
         </div>
       </div>
     );
