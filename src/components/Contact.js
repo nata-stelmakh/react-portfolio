10d7c5f7c3a5b734df10e00605220ef6d076a078
Natallia Stelmakh <stelmakh.natallia@gmail.com>
2020-11-16 00:16:57 -0500

    Added buttons on the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
+import { Grid, Cell, Button } from "react-mdl";
 
 class Contact extends Component {
   render() {
     return (
-      <div>
+      <div style={{ width: "100%", margin: "auto" }}>
         <div className="container" id="mainContainer">
-          <h2>Contact Me</h2>
+          <Grid className="landing-grid">
+            <Cell col={12}>
+              <h2>Contact Me</h2>
 
-          <p>
-            <a id="email" href="mailto:stelmakh.natallia@gmail.com">
-              Email: stelmakh.natallia@gmail.com
-            </a>
-          </p>
-          <p id="phone">Phone :+1-973-7506535</p>
+              <p>
+                <Button>
+                  {" "}
+                  <a id="email" href="mailto:stelmakh.natallia@gmail.com">
+                    Email: stelmakh.natallia@gmail.com
+                  </a>
+                </Button>
+              </p>
+              <p id="phone">
+                <Button>Phone :+1-973-7506535</Button>
+              </p>
+            </Cell>
+          </Grid>
         </div>
       </div>
     );
